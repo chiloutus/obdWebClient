6d8fafd0695155ecf63ebeba0fe953e809cb44de
garylaptop <lynamgaz@yahoo.ie>
2015-03-24 18:22:42 +0000

    Changing str() to repr()

--- commentsapp-C4/commentsapp.py
+++ commentsapp-C4/commentsapp.py
@@ -61,7 +61,7 @@ def vehicleData():
 
     cur = conn.cursor()
     i = session['OwnerId']
-    id = str(i)
+    id = repr(i)
     print(id)
     sql = "SELECT * FROM Car WHERE OwnerId = {}".format(id)
 
